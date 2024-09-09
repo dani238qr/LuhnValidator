d60c07053b1b31e8b17e40fdb07d03fe08c0f0ca
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-09-09 18:00:50 +0300

    Create luhn.c

---
+++ luhn.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+
+void luhn_algh(char num[]);
+
+int main(){
+  
+    char num[20];
+
+    printf("Enter card or IMEI number: ");
+    scanf("%19s", num);
+
+    luhn_algh(num);
+
+
+
+    return 0;
+}
+
+
+void luhn_algh(char num[]){
+    
+    int sum=0;
+    int len=strlen(num);
+
+    for(int i=len-1;i>=0;i--){
+        int n=(num[i]-'0');
+
+        if((len-i)%2==0){
+            n*=2;
+            if(n>9){
+                n-=9;
+            }
+        }
+        sum+=n;
+        
+    }
+
+    if(sum % 10==0){
+        printf("card or IMEI number is valid.\n");
+    } 
+    else{
+        printf("card or IMEI number is invalid.\n");
+    }
+    
+}
